be42bda0fd2b7af9e9af6c793b03067e365aa9f6
uttamsgowdamain <uttamsgowdamain@gmail.com>
2025-09-21 06:53:59 -0700

    Update merge_sort.h

--- Merge Sort/merge_sort.h
+++ Merge Sort/merge_sort.h
@@ -1,9 +1,8 @@
 #ifndef MERGE_SORT_H
 #define MERGE_SORT_H
 
-using namespace std;
-
 void merge(long long int array[], int const left, int const mid, int const right);
+
 void merge_sort(long long int array[], int const begin, int const end);
 
 #endif
